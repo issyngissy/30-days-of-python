37cd3c0156a29970edb7a8e20772918fc01af5ce
issyngissy <issy.ng@icloud.com>
2026-05-29 01:13:35 +0800

    staying up til 1am to figure out litecoin division

--- answers/ex5.py
+++ answers/ex5.py
@@ -1,57 +1,53 @@
-#import requests
-#from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
-#URL = "https://bitbo.io/"
-##page = requests.get(URL)
 
-#soup = BeautifulSoup(page.content, "html.parser")
+URL = "https://bitbo.io/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
 
-#print(page.text)
-#results = soup.find(id="top-stats")
-#print(results.prettify())
-
-#btc_amount = results.find_all("div", class_="amount")
-
-#btc_amount = str(btc_amount)
+results = soup.find(id="top-stats")
+btc_amount = results.find_all("div", class_="amount")
+btc_amount = str(btc_amount)
 
 #-----------------------------
 
-#btc = btc_amount[32:38].strip().replace(",", "")
-
-#btc = int(btc)
-btc = int(74000)
+btc = btc_amount[32:38].strip().replace(",", "")
+btc = int(btc) 
+btc = 75000
 ethereum = int(2777)
 litecoin = int(70)
 
-current_balance = int(100000)
-
+current_balance = int(7548938)
 
 btc_count = int(0)
 ethereum_count = int(0)
-litecoin_count = int(0)
+litecoin_count = float(0)
+
+
+
 while current_balance > btc:
     current_balance -= btc
     btc_count += 1
-        
-    
 
 while current_balance > ethereum:
     current_balance -= ethereum
-    print(current_balance)
-    ethereum_count += 1
-        
+    ethereum_count += 1    
 
 while current_balance > litecoin:
     current_balance -= litecoin
     litecoin_count += 1
-        
+
+litecoin_count += current_balance / litecoin 
+current_balance -= current_balance 
 
 total_crypto_spend = btc_count * btc + ethereum_count * ethereum + litecoin_count * litecoin
 
-
 print("\n", "The amount of crypto you can buy is")
-print("  bitcoin  ", btc_count, "\n", " ethereum ", ethereum_count, "\n", " litecoin ", litecoin_count)
-print("\n", "The amount you will spent ", total_crypto_spend)
+print(" bitcoin  ", btc_count, "      @ price $", btc)
+print(" ethereum ", ethereum_count, "      @ price $", ethereum)
+print(" litecoin ", litecoin_count, "    @ price $", litecoin)
+print("\n", "The amount you will spend ", total_crypto_spend)
 print(" Remaining balance ", current_balance, "\n")
 
 
---
+++ answers/ex6.py
@@ -0,0 +1,4 @@
+x = int(100)
+y = int(50)
+
+print(x + y)
