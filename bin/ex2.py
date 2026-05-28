06def74a78a67c24938f268416bcb2080f9c1f43
issyngissy <issy.ng@icloud.com>
2026-05-28 23:24:06 +0800

    deleted venv files

--- bin/ex2.py
+++
@@ -1,6 +0,0 @@
-Geometry = 6
-Algebra = 7
-Physics = 8
-
-average = (Geometry + Algebra + Physics) / 3
-print(average)
--- bin/ex4.py
+++
@@ -1,5 +0,0 @@
-width = 5
-height = 2
-
-total_area = width * height
-print("total areas is " total_area)
